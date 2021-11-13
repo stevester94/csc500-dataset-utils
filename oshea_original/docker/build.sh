b1ddbd8294a4253eea28d19fc75fb54afb461161
Steven Mackey <stevester94@gmail.com>
2021-11-13 13:42:37 -0800

    Well it makes something...

---
+++ oshea_original/docker/build.sh
@@ -0,0 +1,1 @@
+docker build -t gnuradio-3.7-image .
---
+++ oshea_original/docker/run.sh
@@ -0,0 +1,8 @@
+docker run \
+-ti --rm --network=host \
+--name gnuradio-cont \
+-v /etc/passwd:/etc/passwd \
+-u $(id -u):$(id -g) \
+-v $(realpath ..):/work \
+gnuradio-3.7-image \
+jupyter-notebook --notebook-dir=/work
