715f85b681787c74bc62800c29555b23f6a63cc6
Steven Mackey <stevester94@gmail.com>
2021-11-16 18:38:25 -0800

    .

--- oshea_original/docker/generate_dataset.sh
+++ oshea_original/docker/generate_dataset.sh
@@ -2,6 +2,7 @@ docker run \
 -ti --rm --network=host \
 --name oshea-generator \
 -v /etc/passwd:/etc/passwd \
+-v /etc/group:/etc/group \
 -u $(id -u):$(id -g) \
 -v $(realpath ..):/work \
 gnuradio-3.7-oshea-image \
--- oshea_original/docker/run_companion.bash
+++ oshea_original/docker/run_companion.bash
@@ -2,8 +2,15 @@ docker run \
 -ti --rm --network=host \
 --name oshea-generator \
 -v /etc/passwd:/etc/passwd \
+-v /etc/shadow:/etc/shadow \
+-v /etc/group:/etc/group \
+-v /etc/sudoers:/etc/sudoers \
 -u $(id -u):$(id -g) \
--v $(realpath ..):/work \
+-v $(realpath ../../../csc500-examples/gnuradio_exploration/):/gnuradio_exploration \
+-v $(realpath ../):/work \
 -e DISPLAY \
+-e QT_X11_NO_MITSHM=1 \
+--userns=host --pid=host \
 gnuradio-3.7-oshea-image \
-bash
+gnuradio-companion
+#$!
